3cf1e37c301d2a50a04e34fb5f08faf7fde51654
Andras-corda <andrasgamer568@gmail.com>
2025-12-31 02:46:21 +0100

    -

--- Config/discordConfig.php
+++ Config/discordConfig.php
@@ -4,14 +4,7 @@ define('DISCORD_CLIENT_ID', '1431177167605207070');
 define('DISCORD_CLIENT_SECRET', 'TjwWFMe0MCrEorh3vNOeYmMWAsr1uTiF');
 
 // Déterminer l'URL de redirection selon l'environnement
-if (getenv('HEROKU_APP_NAME')) {
-    // Sur Heroku
-    $appName = getenv('HEROKU_APP_NAME');
-    define('DISCORD_REDIRECT_URI', "https://{$appName}.herokuapp.com/auth/discord/callback");
-} else {
-    // En local - Utilisez votre configuration locale
-    define('DISCORD_REDIRECT_URI', 'http://localhost:3000/index.php?callback=discord');
-}
+define('DISCORD_REDIRECT_URI', "https://blenderheajweb-production.up.railway.app/auth/discord/callback");
 
 // URLs de l'API Discord
 define('DISCORD_AUTH_URL', 'https://discord.com/api/oauth2/authorize');
